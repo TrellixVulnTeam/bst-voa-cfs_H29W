9b4acbb6b93d2257cd95a397823edf4b938dcce2
“dumi72” <69515121+dumi72@users.noreply.github.com>
2021-12-08 17:26:09 +0000

    changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Notify VOA of a property',
 
   // Default port that prototype runs on
   port: '3000',
