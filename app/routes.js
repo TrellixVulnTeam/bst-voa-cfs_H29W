597007b3c4cd629e179a80fe7c32d8b34364d73b
Amolsingh Rajput <amolsingh.rajput@digital.hmrc.gov.uk>
2022-02-23 13:33:35 +0000

    Notify client changes -test only

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,6 @@
+var NotifyClient = require('notifications-node-client').NotifyClient,
+    notify = new NotifyClient(process.env.NOTIFYAPIKEY);
+
 const express = require('express')
 const router = express.Router()
 
@@ -164,5 +167,26 @@ console.log("Confirm rempve = "+ confirmremoverowissue)
   }
 
 })
+
+
+// The URL here needs to match the URL of the page that the user is on
+// when they type in their email address
+router.post('/email-address-page', function (req, res) {
+
+  notify.sendEmail(
+    // this long string is the template ID, copy it from the template
+    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+    // in your code.
+    'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    // `emailAddress` here needs to match the name of the form field in
+    // your HTML page
+    req.body.emailAddress
+  );
+
+  // This is the URL the users will be redirected to once the email
+  // has been sent
+  res.redirect('/confirmation-page');
+
+});
 
 module.exports = router
