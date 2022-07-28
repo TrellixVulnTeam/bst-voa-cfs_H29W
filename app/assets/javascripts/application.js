f967a18febdeee2e97666f4e303208dbf3e2d9d6
amolsingh.rajput@digital.hmrc.gov.uk <amolsingh.rajput@digital.hmrc.gov.uk>
2022-07-28 17:19:56 +0100

    Changes to readmore script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,10 +5,61 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
+
+function toggleText() {
+
+    // Get all the elements from the page
+    var points =
+        document.getElementById("defaulttext");
+
+    var showMoreText =
+        document.getElementById("moreText");
+
+    var buttonText =
+        document.getElementById("readmore");
+
+    // If the display property of the dots
+    // to be displayed is already set to
+    // 'none' (that is hidden) then this
+    // section of code triggers
+    if (points.style.display === "none") {
+
+        // Hide the text between the span
+        // elements
+        showMoreText.style.display = "none";
+
+        // Show the dots after the text
+        points.style.display = "inline";
+
+        // Change the text on button to
+        // 'Show More'
+        buttonText.innerHTML = "Read more...";
+    }
+
+    // If the hidden portion is revealed,
+    // we will change it back to be hidden
+    else {
+
+        // Show the text between the
+        // span elements
+        showMoreText.style.display = "inline";
+
+        // Hide the dots after the text
+        points.style.display = "none";
+
+        // Change the text on button
+        // to 'Show Less'
+        buttonText.innerHTML = "Read Less...";
+    }
+}
+
 $(document).ready(function () {
   window.MOJFrontend.initAll()
   window.GOVUKFrontend.initAll()
 })
+
+
+
 
 new MOJFrontend.FilterToggleButton({
   bigModeMediaQuery: "(min-width: 48.063em)",
